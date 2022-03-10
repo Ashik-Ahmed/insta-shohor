0a79742985baef3c24c55341a7a90a165c12eb30
Ashik-Ahmed <ashikahmed121@gmail.com>
2022-03-11 01:22:53 +0600

    first question added

--- js/app.js
+++ js/app.js
@@ -143,7 +143,7 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
-  document.getElementById("liked").innerHTML = "";
+  document.getElementById("liked").textContent = "";
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById("liked").appendChild(div);
